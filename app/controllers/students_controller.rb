d71a62602a0128e9baa25fee206af49d4ee0a9c3
jamieberrier <berrier.jamie@gmail.com>
2019-11-11 02:59:45 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create()
+    @student = Student.create(first_name: , last_name:)
     redirect_to student_path(@student)
   end
 
