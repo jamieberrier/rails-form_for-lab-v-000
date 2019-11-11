68988cc10e726d29784cbc76882ce20d3dee1b58
jamieberrier <berrier.jamie@gmail.com>
2019-11-11 03:21:18 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,4 +22,10 @@ class StudentsController < ApplicationController
 		@student.update(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
 		redirect_to student_path(@student)
   end
+
+  private
+
+	def student_params(*args)
+	  params.require(:student).permit(*args)
+	end
 end
